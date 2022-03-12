3c256900e4b0110e53aab8329b9d60152b589bd6
km796 <31836577+km796@users.noreply.github.com>
2022-03-12 21:44:06 +0900

    youtube api call

--- videos/src/apis/youtube.js
+++ videos/src/apis/youtube.js
@@ -1,1 +1,14 @@
+import axios from 'axios';
+
+
 const KEY = 'AIzaSyBtwCqWEqDa7ldRzWPvlkUu-F9HTvoC57k'
+
+export default axios.create({
+    baseURL: 'https://www.googleapis.com/youtube/v3',
+    params: {
+        part: 'snippet',
+        type: 'video',
+        maxResults: 5,
+        key: KEY
+    }
+})
