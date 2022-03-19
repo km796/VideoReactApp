b504236701fbb3c557f6ad10ebbd4ed9ee9d6074
km796 <31836577+km796@users.noreply.github.com>
2022-03-19 12:51:35 +0900

    Show video detail on video item click

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -2,13 +2,11 @@ import React from "react";
 import SearchBar from "./SearchBar";
 import youtube from "../apis/youtube";
 import VideoList from "./VideoList";
+import VideoDetail from "./VideoDetail";
 
 class App extends React.Component {
-    state = {videos: []}
+    state = {videos: [], selectedVideo: null}
 
-    onVideoSelected = (video) => {
-        console.log(video)
-    }
 
     onFormSubmit = async term => {
         const response = await youtube.get('/search', {
@@ -19,12 +17,19 @@ class App extends React.Component {
         this.setState({videos: response.data.items})
     }
 
+    onVideoSelected = video => {
+        this.setState({selectedVideo: video});
+    };
+
     render() {
         return (
             <div className="ui container">
                 <SearchBar onFormSubmit={this.onFormSubmit}/>
-                <VideoList videos={this.state.videos}
-                 onVideoSelected = {this.onVideoSelected}/>
+                <VideoDetail video={this.state.selectedVideo} />
+                <VideoList 
+                videos={this.state.videos}
+                 onVideoSelected = {this.onVideoSelected}  
+                 />
             </div>
         )
     }
