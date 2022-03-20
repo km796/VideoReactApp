cdcd88bdbdc92fdd93c2d20207852799f2899c1c
km796 <31836577+km796@users.noreply.github.com>
2022-03-20 16:22:20 +0900

    merge confict resolve

--- videos/src/components/VideoDetail.js
+++ videos/src/components/VideoDetail.js
@@ -4,7 +4,6 @@ const VideoDetail = ({video}) => {
     if (!video) {
         return <div>Loading...</div>
     }
-<<<<<<< HEAD
 
     const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`
 
@@ -14,17 +13,11 @@ const VideoDetail = ({video}) => {
             <iframe title="video player" src={videoSrc} />
 
         </div>
-=======
-    return (
->>>>>>> 62498c9c65d2c3428343da2e9d51fa60f6f4d1f3
         <div className="ui segment">
             <h4 className="ui header">{video.snippet.title}</h4>
             <p>{video.snippet.description}</p>
         </div>
-<<<<<<< HEAD
         </div>
-=======
->>>>>>> 62498c9c65d2c3428343da2e9d51fa60f6f4d1f3
     );
 };
 
